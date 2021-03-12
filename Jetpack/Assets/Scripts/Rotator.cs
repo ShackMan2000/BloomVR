41d802bfb1356129b18213e519eb37fa578587ec
ShackMan <julianpieper@gmx.de>
2021-03-12 14:56:50 +0100

    no message

---
+++ Jetpack/Assets/Scripts/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector3 rotation;
+
+
+
+
+
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    transform.Rotate(rotation, rotation);
+    //}
+}
